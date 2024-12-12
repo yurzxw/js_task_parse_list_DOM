378788f8472b0486174bc31e7a0c61371c9f4998
Yura <yuradziavoruk5@gmail.com>
2024-12-13 01:05:43 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+let employeesList = [...document.querySelectorAll('li')];
+
+const helper = (list) => {
+  return list.map((x) => {
+    return {
+      salary: Number(x.dataset.salary.split(',').join('').slice(1)),
+      element: x,
+    };
+  });
+};
+
+employeesList = helper(employeesList);
+
+const sortList = (list) => {
+  return list.sort((a, b) => b.salary - a.salary);
+};
+
+employeesList = sortList(employeesList);
+
+const parentt = document.querySelector('ul');
+
+parent.innerHTML = '';
+employeesList.forEach((empl) => parentt.appendChild(empl.element));
+
+// const getEmployees = (list) => {};
